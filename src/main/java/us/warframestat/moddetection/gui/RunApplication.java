cbca2a19476373c527686c8225bf6c371aba061e
Tiebe Groosman <tiebe.groosman@gmail.com>
2022-01-10 23:44:59 +0100

    remove some stuff

--- src/main/java/us/warframestat/moddetection/gui/RunApplication.java
+++ src/main/java/us/warframestat/moddetection/gui/RunApplication.java
@@ -1,9 +1,5 @@
 package us.warframestat.moddetection.gui;
 
-import org.bytedeco.javacpp.Loader;
-import org.bytedeco.opencv.global.opencv_core;
-import us.warframestat.moddetection.api.App;
-
 import java.io.IOException;
 
 public class RunApplication {
@@ -13,13 +9,7 @@ public class RunApplication {
    *
    * @param args default args
    */
-  public static void main(String[] args) throws IOException, InterruptedException {
-    try {
-      Loader.load(opencv_core.class);
-    } catch (UnsatisfiedLinkError e) {
-      String path = Loader.cacheResource(opencv_core.class, "windows-x86_64/jni<module>.dll").getPath();
-      new ProcessBuilder("c:/Users/tiebe/Downloads/Dependencies_x64_Release/DependenciesGui.exe", path).start().waitFor();
-    }
+  public static void main(String[] args) {
     MainApplication.main(args);
   }
 }
